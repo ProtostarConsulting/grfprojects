ebc0b66bf002a7cbd2d886c35d3accba2a417901
Sagar Sadawarte <sagar@protostar.co.in>
2017-03-07 18:27:33 +0530

    print address & bookDetails of an school

---
+++ prostudy-grf-ui/app/partnerschool/print-address.page.ts
@@ -0,0 +1,7 @@
+import { Component, Optional } from '@angular/core';
+
+@Component({
+    template : `<h2>Print Address Page</h2> <br>
+            <proerp-print-address> </proerp-print-address>`
+})
+export class PrintAddressPage {}
---
+++ prostudy-grf-ui/app/partnerschool/print-bookdetails.page.ts
@@ -0,0 +1,7 @@
+import { Component, Optional } from '@angular/core';
+
+@Component({
+    template : `<h2>Print BookDetails Page</h2> <br>
+            <proerp-print-bookdetails> </proerp-print-bookdetails>`
+})
+export class PrintBookDetailsPage {}
